7f2cf33f056da387560219d47fc0bc76a90243d6
rezathriii <taheri.reza94@gmail.com>
2025-03-08 02:03:42 +0100

    update http to https

--- api_gateway/app.py
+++ api_gateway/app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 CORS(app)
 
 # Service URLs
-DATA_INTEGRATOR_URL = "http://data_integrator:8761"
-COMPUTATIONS_URL = "http://computations:8762"
+DATA_INTEGRATOR_URL = "https://data_integrator:8761"
+COMPUTATIONS_URL = "https://computations:8762"
 
 # Maximum allowed distance between receivers in kilometers
 MAX_RECEIVER_DISTANCE_KM = 20
